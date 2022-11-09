e1b22866f7f18b7a5630ff282f1f617a36649d8e
Beni Vucsics <vucben99@gmail.com>
2022-11-09 11:55:19 +0100

    Working except search

--- app.js
+++ app.js
@@ -1,2 +1,56 @@
 const root = document.getElementById("root")
+const userCardsContainer = document.createElement("div")
+
+const searchBar = document.createElement("input")
+searchBar.classList = "searchBar"
+searchBar.setAttribute("type", "text")
+searchBar.setAttribute("placeholder", "Search")
+searchBar.addEventListener("input", (event) => {
+    console.log(event.target.value)
+})
+root.appendChild(searchBar)
+
+
+const setDisplay = (event) => {
+
+    if (event.target.nextElementSibling.style.display === "block") {
+        event.target.nextElementSibling.style.display = "none"
+    } else {
+        event.target.nextElementSibling.style.display = "block"
+    }
+}
+
+const init = async () => {
+
+    const response = await fetch(`https://api.github.com/users`, {
+        headers: {
+            authorization: "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"
+        }
+    })
+    const usersData = await response.json()
+
+    userCardsContainer.classList = "userCardsContainer"
+
+    userCardsContainer.innerHTML = usersData.map((user) => {
+        return `
+        <div class="userCard">
+            <img src=${user.avatar_url}>
+            <p>${user.login}</p>
+            <button>Show more</button>
+            <div class="hiddenInfo">
+                <p>Rank: ${user.type}</p>
+                <p>Admin: ${user.site_admin}</p>
+            </div>
+        </div>`
+    }).join("")
+
+    root.appendChild(userCardsContainer)
+
+    const btns = document.getElementsByTagName("button")
+    for (const btn of btns) {
+        btn.addEventListener("click", setDisplay)
+    }
+}
+
+init()
 
